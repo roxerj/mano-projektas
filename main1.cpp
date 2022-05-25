b6de6fced10f7c9177cacda21db224dc4fe225da
roxerj <32255747+roxerj@users.noreply.github.com>
2022-05-25 12:16:03 +0300

    main 03

---
+++ main1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <string.h>
+#include "juozapo.h"
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int n = 100; // studentu skaicius imamas is failo
+    int m = 7;   // namu darbu sk
+    nuskaitymas(n, m);
+
+
+    skaiciavimas(n, m);
+
+    return 0;
+}
+
